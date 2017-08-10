c3023b8eb3a6620c9368ea7f66d843fdf83d96ea
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-10 12:19:43 -0400

    Add '', '/kiosks' and '**'  paths to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { KiosksComponent } from './kiosks/kiosks.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: 'kiosks', pathMatch: 'full' },
+  { path: 'kiosks', component: KiosksComponent },
+  { path: '**', redirectTo: 'kiosks' }
 ];
 
 @NgModule({
