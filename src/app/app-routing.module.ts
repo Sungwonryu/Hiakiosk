22486a477a51d26c90ee7920f7bf1813e0e7f824
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-11 15:31:15 -0400

    Add 'kiosks/' path in app-routing.module.ts and add 'router-outlet' to kiosks.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { KiosksComponent } from './kiosks/kiosks.component';
+import { KioskListComponent } from './kiosks/kiosk-list/kiosk-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'kiosks', pathMatch: 'full' },
-  { path: 'kiosks', component: KiosksComponent },
+  { path: 'kiosks', component: KiosksComponent, children: [
+    { path: '', component: KioskListComponent }
+  ]},
   { path: '**', redirectTo: 'kiosks' }
 ];
 
