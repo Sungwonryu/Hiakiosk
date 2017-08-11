857cc6a5066ec3745fb1f3ff46f3a74dc4e9e1be
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-11 15:40:20 -0400

    Add the codes for loading this.kiosks in kiosk-list.component.ts

--- src/app/kiosks/kiosk-list/kiosk-list.component.ts
+++ src/app/kiosks/kiosk-list/kiosk-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Kiosk } from '../kiosk.model';
+import { KioskService } from '../kiosk.service';
+
 @Component({
   selector: 'hk-kiosk-list',
   templateUrl: './kiosk-list.component.html',
   styleUrls: ['./kiosk-list.component.scss']
 })
 export class KioskListComponent implements OnInit {
+  kiosks: Kiosk[];
 
-  constructor() { }
+  constructor(private kioskService: KioskService) { }
 
   ngOnInit() {
+    this.kioskService.getList().subscribe((list: Kiosk[]) => {
+      this.kiosks = list;
+    });
   }
 
 }
