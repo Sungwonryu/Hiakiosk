71bf93057e9cb71b90d493d3acc5bdbeac8dc859
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-11 16:52:04 -0400

    Add the codes for loading this.kiosk using the resolved route.data

--- src/app/kiosks/kiosk/kiosk.component.ts
+++ src/app/kiosks/kiosk/kiosk.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Data } from '@angular/router';
+
+import { Kiosk } from '../kiosk.model';
 
 @Component({
   selector: 'hk-kiosk',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kiosk.component.scss']
 })
 export class KioskComponent implements OnInit {
+  kiosk: Kiosk;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.route.data.subscribe((data: Data) => {
+      if (data.kiosk) {
+        this.kiosk = data.kiosk;
+      } else {
+        // If kiosk is not found
+        console.log('Kiosk is not found');
+        this.router.navigate(['../'], {relativeTo: this.route});
+      }
+    })
   }
 
 }
