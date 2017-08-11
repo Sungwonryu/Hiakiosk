a9b86a946f420cf9ab6bc9d493467e298e966cee
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-11 16:47:53 -0400

    Add the codes for resolving kiosk data in kiosk-resolver.service.ts

--- src/app/kiosks/kiosk-resolver.service.ts
+++ src/app/kiosks/kiosk-resolver.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { Kiosk } from './kiosk.model';
+import { KioskService } from './kiosk.service';
 
 @Injectable()
-export class KioskResolver {
+export class KioskResolver implements Resolve<Kiosk> {
 
-  constructor() { }
+  constructor(private kioskService: KioskService) { }
 
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Kiosk> | Promise<Kiosk> | Kiosk {
+
+    return this.kioskService.getKioskById(+route.params.kioskId);
+  }
 }
