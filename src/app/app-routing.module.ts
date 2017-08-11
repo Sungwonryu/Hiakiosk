4efee509d1892bbd0956baba98032a55567295e6
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-11 16:50:07 -0400

    Add 'kiosks/:kioskId' path which loads KioskComponent with fetching data with KioskResolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { KiosksComponent } from './kiosks/kiosks.component';
 import { KioskListComponent } from './kiosks/kiosk-list/kiosk-list.component';
+import { KioskComponent } from './kiosks/kiosk/kiosk.component';
+
+import { KioskResolver } from './kiosks/kiosk-resolver.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'kiosks', pathMatch: 'full' },
   { path: 'kiosks', component: KiosksComponent, children: [
-    { path: '', component: KioskListComponent }
+    { path: '', component: KioskListComponent },
+    { path: ':kioskId', component: KioskComponent, resolve: { kiosk: KioskResolver } }
   ]},
   { path: '**', redirectTo: 'kiosks' }
 ];
