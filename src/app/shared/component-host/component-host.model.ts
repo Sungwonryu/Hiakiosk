479c562413f8f47dba40240e07852e85ffb448aa
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-23 17:00:46 -0400

    Clean up the codes and add comments in component-host.model.ts

--- src/app/shared/component-host/component-host.model.ts
+++ src/app/shared/component-host/component-host.model.ts
@@ -1,38 +1,16 @@
-// import { Type } from '@angular/core';
-//
-// export class ComponentHost {
-//   public componentId: number;
-//   public componentName: string | null | undefined;
-//   public component: Type<any> | null | undefined;
-//   public data: any;
-//
-//   constructor({ componentId, componentName, component, data }:
-//     {
-//       componentId: number,
-//       componentName?: string,
-//       component?: Type<any>,
-//       data?: any
-//     }) {
-//
-//     this.set({ componentId, componentName, component, data });
-//   }
-//
-//   set({ componentId, componentName, component, data }:
-//     {
-//       componentId?: number | null | undefined,
-//       componentName?: string | null | undefined,
-//       component?: Type<any> | null | undefined,
-//       data?: any | null | undefined
-//     }) {
-//
-//     if (componentId !== null) this.componentId = componentId;
-//     if (componentName !== null) this.componentName = componentName;
-//     if (component !== null) this.component = component;
-//     if (data !== null) this.data = data;
-//   }
-// }
 import { Type } from '@angular/core';
 
+/**
+ *  param {Number} componentId
+ *  param {String} componentName
+ *  param {Type} component
+ *  param {Object} data
+ *
+ *  componentId - unique integer ID for component which starting from 1
+ *  componentName - unique name for component
+ *  component - component object
+ *  data
+ */
 export class ComponentHost {
   public componentId: number;
   public componentName: string | null | undefined;
