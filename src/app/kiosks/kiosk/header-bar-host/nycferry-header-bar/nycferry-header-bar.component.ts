afda2d81849e5b2e3f2b79df7803379933de3471
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-23 17:12:50 -0400

    Add NycferryHeaderBar component by using "ng g c kiosks/kiosk/header-bar-host/nycferry-header-bar"

---
+++ src/app/kiosks/kiosk/header-bar-host/nycferry-header-bar/nycferry-header-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hk-nycferry-header-bar',
+  templateUrl: './nycferry-header-bar.component.html',
+  styleUrls: ['./nycferry-header-bar.component.scss']
+})
+export class NycferryHeaderBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
