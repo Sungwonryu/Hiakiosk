c78389c128b92f7b427c714b0631cc6684c1480b
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-23 17:10:34 -0400

    Add header-bar-host.component.ts

---
+++ src/app/kiosks/kiosk/header-bar-host/header-bar-host.component.ts
@@ -0,0 +1,49 @@
+import {
+  Component,
+  Input,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ComponentFactoryResolver,
+  Type
+} from '@angular/core';
+
+import { Kiosk } from '../../kiosk.model';
+import { HeaderBar } from './header-bar.model';
+import { HeaderBarService } from './header-bar.service';
+
+import { ComponentHostDirective } from '../../../shared/component-host/component-host.directive';
+import { ComponentHostInterface } from '../../../shared/component-host/component-host.interface';
+import { ComponentHost } from '../../../shared/component-host/component-host.model';
+
+
+@Component({
+  selector: 'hk-header-bar-host',
+  templateUrl: './header-bar-host.component.html',
+  styleUrls: ['./header-bar-host.component.scss']
+})
+export class HeaderBarHostComponent implements OnInit, AfterViewInit {
+  @Input('kiosk') kiosk: Kiosk;
+  @ViewChild(ComponentHostDirective) componentHostEl: ComponentHostDirective;
+  componentHost: HeaderBar;
+
+  constructor(
+    private headerBarService: HeaderBarService,
+    private componentFactoryResolver: ComponentFactoryResolver) { }
+
+  ngOnInit() {
+    this.componentHost = this.headerBarService.getComponentHost(this.kiosk.headerBar.componentId);
+  }
+
+  ngAfterViewInit() {
+    this.loadComponent();
+  }
+
+  loadComponent() {
+    let componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.componentHost.component);
+    let viewContainerRef = this.componentHostEl.viewContainerRef;
+    viewContainerRef.clear();
+    let componentRef = viewContainerRef.createComponent(componentFactory);
+    (<ComponentHostInterface>componentRef.instance).data = this.componentHost.data;
+  }
+}
