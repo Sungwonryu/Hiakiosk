4c973b81e167efdf6b4ee994992e07b1912209e8
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-25 20:06:41 -0400

    Modify KioskService to use HttpClient instead of Http
    . Replace http to httpClient
    . Add getMatchedKioskGroup() and getMatchedKiosk()
    . Use forkJoin() to load kiosk-list.json and kiosk-group-list.json files together
    . Make to check the existence of this.kioskList before making http calls

--- src/app/kiosks/kiosk.service.ts
+++ src/app/kiosks/kiosk.service.ts
@@ -1,67 +1,134 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+// import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/forkJoin';
 
 import { Kiosk } from './kiosk.model';
+import { KioskGroup } from './kiosk-group.model';
 
 @Injectable()
 export class KioskService {
-  path = '../../assets/json/';
-  fileName = 'kiosk-list.json';
-  url = `${this.path}${this.fileName}`;
+  kioskList: Kiosk[];
+  kioskGroupList: KioskGroup[];
 
-  constructor(private http: Http) {}
+  // constructor(private http: Http) {
+  constructor(private httpClient: HttpClient) { }
+
+  /**
+   *  GetData() will return an Observable
+   *  which will return an array of data objects for each http call
+   */
+  getData(): Observable<any> {
+    const jsonPath = '../../assets/json/';
+    const kioskListFile = 'kiosk-list.json';
+    const kioskGroupListFile = 'kiosk-group-list.json';
+    const kioskListUrl = `${jsonPath}${kioskListFile}`;
+    const kioskGroupListUrl = `${jsonPath}${kioskGroupListFile}`;
+
+    return Observable.forkJoin([
+      // this.http.get(kioskListUrl).map((res: Response) => res.json().kioskList),
+      // this.http.get(kioskGroupListUrl).map((res: Response) => res.json().kioskGroupList)
+      this.httpClient.get<any>(kioskListUrl).map((res) => res.kioskList),
+      this.httpClient.get<any>(kioskGroupListUrl).map((res) => res.kioskGroupList)
+    ]);
+  }
 
   /**
    *  GetList() will return an Observable
    *  which will return an array of Kiosk instances
+   *  after setting kioskGroup properties
    */
-  getList(): Observable<any> {
-    return this.http.get(this.url)
-      .map((res: Response) => {
-        return res.json().data.map((settings) => {
-          return new Kiosk(settings);
+  getList(): Observable<Kiosk[]> {
+    if (this.kioskList) {
+      // If this.kioskList exists,
+      // return an Observable which will return an array of Kiosk instances
+      return Observable.of(this.kioskList);
+    } else {
+      // If this.kioskList doesn't exist,
+      // return an Observable getting from this.getData()
+      return this.getData().map((data: any[]) => {
+        let kioskSettingsList = data[0];
+        let kioskGroupSettingsList = data[1];
+
+        this.kioskList = kioskSettingsList.map((kioskSettings) => {
+          let matchedKioskGroup = this.getMatchedKioskGroup(kioskGroupSettingsList, kioskSettings.kioskGroup.kioskGroupId);
+          kioskSettings.kioskGroup = matchedKioskGroup;
+
+          return new Kiosk(kioskSettings);
         });
+
+        return this.kioskList;
       });
+    }
   }
 
   /**
    *  getKiosk() will return an Observable
-   *  which will return undeinfed or a Kiosk instance
-   *  whose id or name is matched with the param, id
+   *  which will return undefined or the instance of Kiosk
+   *  whose id or name is matched with the param, kioskId
    *
-   *  param {string} id
+   *  param {string} kioskId
    */
-  getKiosk(id: string): Observable<any> {
-    return this.http.get(this.url)
-      .map((res: Response) => {
-        const data = res.json().data;
-        let matchedSettings: any;
-        let matchedKiosk: Kiosk | undefined;
-
-        // Check if id is a postive integer in string type
-        if (id.length > 0 && Number.isInteger(+id) && +id > 0) {
-          matchedSettings = data.find((settings) => {
-            return settings.id === +id;
-          });
-        }
-
-        // If matchedSettings is not found by comparing settings.id,
-        // compare id with settings.name
-        if (!matchedSettings) {
-          matchedSettings = data.find((settings) => {
-            return settings.name.toLowerCase() === id.toLowerCase();
-          });
-        }
-
-        // If matchedSettings is found,
-        // create an instance of Kiosk using the matchedSettings and return it
-        // Otherwise undefined will be returned
-        if (matchedSettings) {
-          matchedKiosk = new Kiosk(matchedSettings);
-        }
-        return matchedKiosk;
+  getKiosk(kioskId: string): Observable<any> {
+    if (this.kioskList) {
+      return Observable.of(this.getMatchedKiosk(this.kioskList, kioskId));
+    } else {
+      this.getList().map((kioskList: Kiosk[]) => {
+        return this.getMatchedKiosk(kioskList, kioskId);
       });
+    }
+  }
+
+  /**
+   *  getMatchedKioskGroup() will return the kioskGroup
+   *  whose id is matched with the parameter kioskId
+   *
+   *  param {string} kioskId
+   */
+  private getMatchedKioskGroup(kioskGroupSettingsList: any[], kioskGroupId: string) {
+    let matchedKioskGroupSettings = kioskGroupSettingsList.find((kioskGroupSettings) => {
+      return kioskGroupSettings.id === kioskGroupId;
+    });
+
+    if (matchedKioskGroupSettings) {
+      return new KioskGroup(matchedKioskGroupSettings);
+    } else {
+      console.log('matchedKioskGroupSettings is not found by using kioskGroupId: ', kioskGroupId);
+    }
+  }
+
+  /**
+   *  getMatchedKiosk() will return undefined or the instance of Kiosk
+   *  whose id or name is matched with the parameter kioskId
+   *
+   *  param {string} kioskId
+   */
+  private getMatchedKiosk(kioskList: Kiosk[], kioskId: string) {
+    let matchedKiosk;
+
+    // Check if kioskId is a postive integer in string type
+    if (kioskId.length > 0 && Number.isInteger(+kioskId) && +kioskId > 0) {
+      matchedKiosk = kioskList.find((kiosk) => {
+        return kiosk.id === +kioskId;
+      });
+    }
+
+    // If the matchedKiosk is not found by comparing kioskId and kiosk.id,
+    // compare kioskId with kiosk.name
+    if (!matchedKiosk) {
+      matchedKiosk = kioskList.find((kiosk: Kiosk) => {
+        return kiosk.name.toLowerCase() === kioskId.toLowerCase();
+      });
+    }
+
+    // If the matchedKiosk is found, the matchedKisok will be returned
+    // Otherwise undefined will be returned
+    if (!matchedKiosk) {
+      console.log('matchedKiosk is not found by using kioskId: ', kioskId);
+    }
+    return matchedKiosk;
   }
 }
