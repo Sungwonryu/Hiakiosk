358eea375285ef9d2c894a983808bd8f4e1a60f3
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-25 20:04:37 -0400

    Replace HttpModue to HttpClientModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +35,7 @@ import { HeaderBarService } from './kiosks/kiosk/header-bar-host/header-bar.serv
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
