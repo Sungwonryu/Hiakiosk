e723c8b4e49f593b43e58a3720493e4192998bc8
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-08-29 12:07:28 -0400

    Add the codes for checking kioskId params in resolve()

--- src/app/kiosks/kiosk-resolver.service.ts
+++ src/app/kiosks/kiosk-resolver.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Resolve, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 import { Kiosk } from './kiosk.model';
 import { KioskService } from './kiosk.service';
@@ -8,21 +9,23 @@ import { KioskService } from './kiosk.service';
 @Injectable()
 export class KioskResolver implements Resolve<any> {
 
-
-  constructor(private kioskService: KioskService,
-              private router: Router) { }
+  constructor(
+    private kioskService: KioskService,
+    private router: Router) { }
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any> | Promise<any> | any {
-    let id = route.params.kioskId;
 
-    return this.kioskService.getKiosk(id).map((kiosk: Kiosk | null | undefined) => {
+    let kioskId = route.params.kioskId;
+    return this.kioskService.getKiosk(kioskId).map((kiosk: Kiosk | null | undefined) => {
+      // If the matched kiosk is found, return the matched kiosk,
+      // otherwise navigate to '../'
       if (kiosk) {
         return kiosk;
       } else {
-        this.router.navigate(['/']);
+        this.router.navigate(['../']);
       }
     });
   }
