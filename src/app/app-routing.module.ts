2b67d1e15573fcbfdedbda8d953d909050f4a955
Sungwon  Ryu <sungwonryu@Sungwons-MacBook-Pro.local>
2017-09-13 11:19:04 -0400

    Assign KioskGroupListComponent to root path in app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { KioskGroupListComponent } from './kiosks/kiosk-group-list/kiosk-group-list.component';
 import { KiosksComponent } from './kiosks/kiosks.component';
 import { KioskListComponent } from './kiosks/kiosk-list/kiosk-list.component';
 import { KioskComponent } from './kiosks/kiosk/kiosk.component';
@@ -8,11 +9,13 @@ import { KioskComponent } from './kiosks/kiosk/kiosk.component';
 import { KioskResolver } from './kiosks/kiosk-resolver.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'kiosks', pathMatch: 'full' },
-  { path: 'kiosks', component: KiosksComponent, children: [
-    { path: '', component: KioskListComponent },
-    { path: ':kioskId', component: KioskComponent, resolve: { kiosk: KioskResolver } }
-  ]},
+  { path: '', component: KioskGroupListComponent, pathMatch: 'full' },
+  {
+    path: 'kiosks', component: KiosksComponent, children: [
+      { path: '', component: KioskListComponent },
+      { path: ':kioskId', component: KioskComponent, resolve: { kiosk: KioskResolver } }
+    ]
+  },
   { path: '**', redirectTo: 'kiosks' }
 ];
 
