11bb893060df1a1f5fc0f6a532c6859b56613d69
Admin <admin@Admins-MacBook-Pro.local>
2017-09-14 22:05:19 -0400

    Move header-bar-host.service.ts from kiosks/kiosk/header-bar-host/ to kiosks/kiosk-group/kiosk/header-bar-host/

--- src/app/kiosks/kiosk-group/kiosk/header-bar-host/header-bar-host.service.ts
+++ src/app/kiosks/kiosk-group/kiosk/header-bar-host/header-bar-host.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ComponentHost } from '../../../shared/component-host/component-host.model';
-import { ComponentHostService } from '../../../shared/component-host/component-host.service';
+import { ComponentHost } from '../../../../shared/component-host/component-host.model';
+import { ComponentHostService } from '../../../../shared/component-host/component-host.service';
 
 import { NycferryHeaderBarComponent } from './nycferry-header-bar/nycferry-header-bar.component';
 
