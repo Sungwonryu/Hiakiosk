159c5365d02fe603494b4d52d94e7a03a109f7fd
Admin <admin@Admins-MacBook-Pro.local>
2017-09-14 22:10:05 -0400

    Modify app-routing.module.ts to add the follwoing paths . kiosks . kiosks/:kioskGroupId . kiosks/:kioskGroupId/:kioskId

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { KioskGroupListComponent } from './kiosks/kiosk-group-list/kiosk-group-list.component';
 import { KiosksComponent } from './kiosks/kiosks.component';
-import { KioskListComponent } from './kiosks/kiosk-list/kiosk-list.component';
-import { KioskComponent } from './kiosks/kiosk/kiosk.component';
+import { KioskGroupListComponent } from './kiosks/kiosk-group-list/kiosk-group-list.component';
+import { KioskGroupComponent } from './kiosks/kiosk-group/kiosk-group.component';
+import { KioskListComponent } from './kiosks/kiosk-group/kiosk-list/kiosk-list.component';
+import { KioskComponent } from './kiosks/kiosk-group/kiosk/kiosk.component';
 
-import { KioskResolver } from './kiosks/kiosk-resolver.service';
+import { KioskResolver } from './kiosks/services/kiosk-resolver.service';
+import { KioskGroupResolver } from './kiosks/services/kiosk-group-resolver.service';
 
 const routes: Routes = [
-  { path: '', component: KioskGroupListComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'kiosks', pathMatch: 'full' },
   {
     path: 'kiosks', component: KiosksComponent, children: [
-      { path: '', component: KioskListComponent },
-      { path: ':kioskId', component: KioskComponent, resolve: { kiosk: KioskResolver } }
+      { path: '', component: KioskGroupListComponent },
+      {
+        path: ':kioskGroupId', component: KioskGroupComponent, children: [
+          { path: '', component: KioskListComponent, resolve: { kioskGroup: KioskGroupResolver } },
+          { path: ':kioskId', component: KioskComponent, resolve: { kioskData: KioskResolver } }
+        ]
+      }
     ]
   },
   { path: '**', redirectTo: 'kiosks' }
