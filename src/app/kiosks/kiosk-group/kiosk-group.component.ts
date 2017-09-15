362a737fd76233c00f32648f54aa0527bc3a6faf
Admin <admin@Admins-MacBook-Pro.local>
2017-09-14 21:42:42 -0400

    . Add KioskGroupComponent by using 'ng g c kiosks/kiosk-group --spec false' . Add router-outlet in kiosk-group.component.html

---
+++ src/app/kiosks/kiosk-group/kiosk-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hk-kiosk-group',
+  templateUrl: './kiosk-group.component.html',
+  styleUrls: ['./kiosk-group.component.scss']
+})
+export class KioskGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
