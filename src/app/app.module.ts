67124db1d877d305b96602fa32d147ba3e20238d
Admin <admin@Admins-MacBook-Pro.local>
2017-09-14 22:08:22 -0400

    Update app.module.ts according to the changes on components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // kiosk
-import { KioskGroupListComponent } from './kiosks/kiosk-group-list/kiosk-group-list.component';
 import { KiosksComponent } from './kiosks/kiosks.component';
-import { KioskListComponent } from './kiosks/kiosk-list/kiosk-list.component';
-import { KioskComponent } from './kiosks/kiosk/kiosk.component';
-
-// headerBar
-import { HeaderBarHostComponent } from './kiosks/kiosk/header-bar-host/header-bar-host.component';
-import { NycferryHeaderBarComponent } from './kiosks/kiosk/header-bar-host/nycferry-header-bar/nycferry-header-bar.component';
+import { KioskGroupListComponent } from './kiosks/kiosk-group-list/kiosk-group-list.component';
+import { KioskGroupComponent } from './kiosks/kiosk-group/kiosk-group.component';
+import { KioskListComponent } from './kiosks/kiosk-group/kiosk-list/kiosk-list.component';
+import { KioskComponent } from './kiosks/kiosk-group/kiosk/kiosk.component';
 
 // shared
 import { ComponentHostDirective } from './shared/component-host/component-host.directive';
 
+// headerBar
+import { HeaderBarHostComponent } from './kiosks/kiosk-group/kiosk/header-bar-host/header-bar-host.component';
+import { NycferryHeaderBarComponent } from './kiosks/kiosk-group/kiosk/header-bar-host/nycferry-header-bar/nycferry-header-bar.component';
+
 // services
-import { KioskService } from './kiosks/kiosk.service';
-import { KioskResolver } from './kiosks/kiosk-resolver.service';
-import { HeaderBarHostService } from './kiosks/kiosk/header-bar-host/header-bar-host.service';
+import { KioskService } from './kiosks/services/kiosk.service';
+import { KioskResolver } from './kiosks/services/kiosk-resolver.service';
+import { KioskGroupResolver } from './kiosks/services/kiosk-group-resolver.service';
+import { HeaderBarHostService } from './kiosks/kiosk-group/kiosk/header-bar-host/header-bar-host.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     KiosksComponent,
+    KioskGroupListComponent,
+    KioskGroupComponent,
     KioskListComponent,
     KioskComponent,
     HeaderBarHostComponent,
     NycferryHeaderBarComponent,
-    ComponentHostDirective,
-    KioskGroupListComponent
+    ComponentHostDirective
   ],
   imports: [
     BrowserModule,
@@ -42,6 +45,7 @@ import { HeaderBarHostService } from './kiosks/kiosk/header-bar-host/header-bar-
   ],
   providers: [
     KioskService,
+    KioskGroupResolver,
     KioskResolver,
     HeaderBarHostService
   ],
