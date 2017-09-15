a779b20e02a3f9ce59d25afe68d18e9f8f974dde
Admin <admin@Admins-MacBook-Pro.local>
2017-09-14 20:54:44 -0400

    . Change the Kiosk class properties to use the newly added KioskConfig model . Move kiosk.model.ts from kiosks/ to kiosks/model/ directory

---
+++ src/app/kiosks/models/kiosk.model.ts
@@ -0,0 +1,47 @@
+import { KioskConfig } from './kiosk-config.model';
+
+/**
+ *  param {Number} id
+ *  param {String} name
+ *  param {Object} kioskConfig
+ *  param {Object} data
+ *
+ *
+ *  id - unique integer ID for kiosk which starting from 1
+ *  name - unique name for kiosk (e.g. 'per11', 'nwill')
+ *  headerBar
+ *  topBar
+ *  bottomBar
+ *  appBar
+ *  data
+ */
+export class Kiosk {
+  public id: number;
+  public name?: string;
+  public kioskConfig?: KioskConfig;
+  public data?: any;
+
+  constructor({ id, name, kioskConfig, data = {} }:
+    {
+      id: number,
+      name?: string,
+      kioskConfig?: KioskConfig,
+      data?: any
+    }) {
+
+    this.set({ id, name, kioskConfig, data })
+  }
+
+  set({ id, name, kioskConfig, data }: {
+    id?: number,
+    name?: string,
+    kioskConfig?: KioskConfig,
+    data?: any
+  }) {
+
+    if (id != null) this.id = id;
+    if (name != null) this.name = name;
+    if (kioskConfig != null) this.kioskConfig = kioskConfig;
+    if (data != null) this.data = data;
+  }
+}
